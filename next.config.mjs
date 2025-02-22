a0782c3357ad411eab817092e96ff13fc84c58b6
Goliaf <ifksiz5@gmail.com>
2025-02-22 18:01:25 +0300

    alowed CORS

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,28 @@ const nextConfig = {
   assetPrefix: process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "",
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Разрешаем запросы с любых доменов
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
