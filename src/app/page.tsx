b3872d6e839c0a4cc51474ecaf76cdb9ad037f49
Goliaf <ifksiz5@gmail.com>
2025-02-23 12:21:38 +0300

    fixed mobile and tab styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import TemplateCard from "../app/components/template-card/TemplateCard";
 import MySwiper from "./components/swiper/Swiper";
 import VideoLogo from "./components/video-logo/VideoLogo";
 
-export default function page() {
+export default function Page() {
   const templates = [
     {
       id: 1,
       title: "Пилотный протокол",
       description: "Идеально подходит для сложных операций.",
-      modelUrl: "/models/scene.gltf", // Путь к модели
+      modelUrl: "/models/scene.gltf",
     },
     {
       id: 2,
@@ -27,37 +27,35 @@ export default function page() {
   ];
 
   return (
-    <div className="min-h-screen width-screen flex flex-col items-center justify-evenly text-white">
+    <div className="min-h-screen w-full flex flex-col items-center justify-evenly text-white">
       {/* Герой-секция */}
-      <section className="container py-20 text-center h-screen flex flex-col items-center justify-around relative">
-        <h1 className="text-6xl font-bold mb-4 animate-fade-in">
+      <section className="container max-w-7xl px-4 sm:px-6 lg:px-8 py-20 text-center h-auto min-h-screen flex flex-col items-center justify-around relative">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 animate-fade-in">
           Хирургические шаблоны нового поколения
         </h1>
-        <p className="text-xl text-[var(--color-text-primary)] mb-8 animate-fade-in-delay">
+        <p className="text-lg sm:text-xl text-[var(--color-text-primary)] mb-8 animate-fade-in-delay">
           Точность, надежность, инновации
         </p>
         <VideoLogo />
       </section>
 
-      {/* Swiper */}
-      {/*  <section className="h-screen flex flex-col items-center justify-center">
-        <MySwiper />
-      </section> */}
       {/* Секция с карточками шаблонов */}
-      <section className="container py-20 text-center h-screen flex flex-col items-center justify-around">
-        <h2 className="text-6xl font-bold mb-4 animate-fade-in">
-          Поробуйте сейчас
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <section className="container max-w-7xl px-4 sm:px-6 lg:px-8 py-20 text-center h-auto min-h-screen flex flex-col items-center justify-around">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 animate-fade-in">
+          Попробуйте сейчас
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {templates.map((template) => (
             <TemplateCard key={template.id} template={template} />
           ))}
         </div>
       </section>
-      <section className="container h-screen py-20 text-center flex flex-col items-center justify-around">
-        <h2 className="text-6xl font-bold mb-4 animate-fade-in">
+
+      {/* Секция с отзывами */}
+      <section className="container max-w-7xl px-4 sm:px-6 lg:px-8 h-auto min-h-screen py-20 text-center flex flex-col items-center justify-around">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 animate-fade-in">
           Отзывы врачей
-        </h2>
+        </h1>
         <MySwiper />
       </section>
     </div>
